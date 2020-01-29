406a3a1f6d971dc755bbe39aeaf077a0f55a4abe
PGRE <hwshadow@gmail.com>
2020-01-29 02:29:50 -0600

    Create launch-daemons.sh
    Dedup logic

---
+++ launch-daemons.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+cd $HOME
+dc_letter_code=$(hostname | awk '{print $2}')
+config_region=$(echo $dc_letter_code | sed 's/^a$/east/g;s/^a$/west/g')
+
+
+# Form Consul Cluster
+ps -C consul
+retval=$?
+if [ $retval -eq 0 ]; then
+  sudo killall consul
+fi
+sudo cp /vagrant/consul-config/consul-server-east.hcl /etc/consul.d/consul-server-$config_region.hcl
+sudo nohup consul agent --config-file /etc/consul.d/consul-server-$config_region.hcl &>$HOME/consul.log &
+
+# Form Nomad Cluster
+ps -C nomad
+retval=$?
+if [ $retval -eq 0 ]; then
+  sudo killall nomad
+fi
+sudo cp /vagrant/nomad-config/nomad-server-east.hcl /etc/nomad.d/nomad-server-$config_region.hcl
+sudo nohup nomad agent -config /etc/nomad.d/nomad-server-$config_region.hcl &>$HOME/nomad.log &
+ps -C consul
+ps -C nomad
